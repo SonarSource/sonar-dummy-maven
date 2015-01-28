e46685e3cfd0e15e456abe88797b3bf40c12885d
Thomas Vérin <thomas.verin@steria.ch>
2015-01-28 15:37:23 +0100

    added package-info

---
+++ sonar-dummy-plugin/src/main/java/com/sonarsource/dummy/plugin/package-info.java
@@ -0,0 +1,7 @@
+/*
+ * Copyright (C) 2014-2014 SonarSource SA
+ * All rights reserved
+ * mailto:contact AT sonarsource DOT com
+ */
+@javax.annotation.ParametersAreNonnullByDefault
+package com.sonarsource.dummy.plugin;
