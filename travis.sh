41f8e323dac2f1c29e5073ecd0e6af4097d6b486
tomverin <thomas.verin@sonarsource.com>
2015-06-16 20:14:36 +0200

    removed -Panalysis

--- travis.sh
+++ travis.sh
@@ -5,7 +5,7 @@ mvn sonar:sonar \
  -Dsonar.analysis.mode=incremental \
  -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
  -Dsonar.github.repository=$SONAR_GITHUB_REPOSITORY \
- -Panalysis -Dsonar.forceUpdate=true \
+ -Dsonar.forceUpdate=true \
  -Dsonar.github.login=$SONAR_GITHUB_LOGIN \
  -Dsonar.github.oauth=$SONAR_GITHUB_OAUTH \
  -Dsonar.host.url=$SONAR_HOST_URL \
