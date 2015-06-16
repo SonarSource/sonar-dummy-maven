060dfb215724fb6cbf141aa61db5b6dab629c76e
Thomas Vérin <thomas.verin@sonarsource.com>
2015-06-16 11:49:32 +0200

    updated travis script

--- travis.sh
+++ travis.sh
@@ -1,15 +1,22 @@
-echo ${sonar.host.url} ${sonar.github.repository} ${TRAVIS_PULL_REQUEST}
+#!/bin/bash
+export SONAR_HOST_URL=localhost:9000
+export SONAR_GITHUB_REPOSITORY=git@github.com:SonarSource/sonar_dummy
+export TRAVIS_PULL_REQUEST=1
+SONAR_GITHUB_LOGIN
+SONAR_GITHUB_OAUTH
+
+echo $SONAR_HOST_URL $SONAR_GITHUB_REPOSITORY $TRAVIS_PULL_REQUEST
 
 mvn sonar:sonar \
  -Dsonar.analysis.mode=incremental \
- -Dsonar.github.pullRequest=${TRAVIS_PULL_REQUEST} \
- -Dsonar.github.repository=${sonar.github.repository} \
+ -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
+ -Dsonar.github.repository=$SONAR_GITHUB_REPOSITORY \
  -Panalysis -Dsonar.forceUpdate=true \
- -Dsonar.github.login=${sonar.github.login} \
- -Dsonar.github.oauth=${sonar.github.oauth} \
- -Dsonar.host.url=${sonar.host.url} \
- -Dsonar.login=${sonar.login} \
- -Dsonar.password=${sonar.password} \
- -Dsonar.jdbc.url=${sonar.jdbc.url} \
- -Dsonar.jdbc.username=${sonar.jdbc.username} \
- -Dsonar.jdbc.password=${sonar.jdbc.password}
+ -Dsonar.github.login=$SONAR_GITHUB_LOGIN \
+ -Dsonar.github.oauth=$SONAR_GITHUB_OAUTH \
+ -Dsonar.host.url=$SONAR_HOST_URL \
+ -Dsonar.login=$SONAR_LOGIN \
+ -Dsonar.password=$SONAR_PASSWORD \
+ -Dsonar.jdbc.url=$SONAR_JDBC_URL \
+ -Dsonar.jdbc.username=$SONAR_JDBC_USERNAME \
+ -Dsonar.jdbc.password=$SONAR_JDBC_PASSWORD
