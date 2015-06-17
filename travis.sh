7c5880e4829c0a31aa34706185bf43f72c8d2d23
tomverin <thomas.verin@sonarsource.com>
2015-06-17 17:57:30 +0200

    changed analysis mode to preview, removed db connection parameters

--- travis.sh
+++ travis.sh
@@ -2,7 +2,7 @@
 echo $SONAR_HOST_URL $SONAR_GITHUB_REPOSITORY $TRAVIS_PULL_REQUEST
 
 mvn sonar:sonar \
- -Dsonar.analysis.mode=incremental \
+ -Dsonar.analysis.mode=preview \
  -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
  -Dsonar.github.repository=$SONAR_GITHUB_REPOSITORY \
  -Dsonar.forceUpdate=true \
@@ -10,7 +10,5 @@ mvn sonar:sonar \
  -Dsonar.github.oauth=$SONAR_GITHUB_OAUTH \
  -Dsonar.host.url=$SONAR_HOST_URL \
  -Dsonar.login=$SONAR_LOGIN \
- -Dsonar.password=$SONAR_PASSWORD \
- -Dsonar.jdbc.url=$SONAR_JDBC_URL \
- -Dsonar.jdbc.username=$SONAR_JDBC_USERNAME \
- -Dsonar.jdbc.password=$SONAR_JDBC_PASSWORD
+ -Dsonar.password=$SONAR_PASSWORD 
+ 
