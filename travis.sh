d12b2faf0be7fcf3e5c9d36f4e6c6df0e03677bd
tomverin <thomas.verin@sonarsource.com>
2015-06-18 11:12:26 +0200

    detect if we are started from a pullrequest in travis

--- travis.sh
+++ travis.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
-echo $SONAR_HOST_URL $SONAR_GITHUB_REPOSITORY $TRAVIS_PULL_REQUEST
-
-mvn sonar:sonar \
+#!/bin/sh
+if [ "${TRAVIS_PULL_REQUEST}" != "false" ] 
+then
+	echo "Start pullrequest analysis"
+ mvn sonar:sonar \
  -Dsonar.analysis.mode=preview \
  -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
  -Dsonar.github.repository=$SONAR_GITHUB_REPOSITORY \
@@ -9,4 +10,4 @@ mvn sonar:sonar \
  -Dsonar.github.login=$SONAR_GITHUB_LOGIN \
  -Dsonar.github.oauth=$SONAR_GITHUB_OAUTH \
  -Dsonar.host.url=$SONAR_HOST_URL 
- 
+fi 
