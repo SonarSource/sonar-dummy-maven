e066cd03d27fef4f9e31f132f29a732b306831c9
Tom <thomas.verin@sonarsource.com>
2017-02-07 13:33:22 +0100

    use stats profile

--- travis.sh
+++ travis.sh
@@ -11,17 +11,6 @@ function installTravisTools {
 installTravisTools
 . installJDK8
 
-#download maven profile
-curl -O http://repo.maven.apache.org/maven2/io/tesla/profile/tesla-profiler/0.0.3/tesla-profiler-0.0.3.jar
-#copy maven to be able to install ext-lib as non root
-mkdir -p ~/maven
-cp -R /usr/local/maven-3.2.5 ~/maven
-cp tesla-profiler-0.0.3.jar ~/maven/maven-3.2.5/lib/ext
-#setup system to use mvn copy
-export M2_HOME=~/maven/maven-3.2.5
-export PATH=$M2_HOME/bin:$PATH
-
-
 java -version
 
 case "$TEST" in
@@ -35,12 +24,14 @@ ci)
   
   export MAVEN_OPTS="-Xmx1536m -Xms128m"
   mvn org.jacoco:jacoco-maven-plugin:prepare-agent deploy sonar:sonar \
-      -Pdeploy-sonarsource,release \
+      -Pdeploy-sonarsource,release,stats \
       -Dmaven.test.redirectTestOutputToFile=false \
       -Dsonar.host.url=$SONAR_HOST_URL \
       -Dsonar.login=$SONAR_TOKEN \
       -Dsonar.projectVersion=$CURRENT_VERSION \
       -B -e -V -Dmaven.profile
+
+  cat stats.log      
   ;;
 
 *)
