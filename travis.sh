6fe616b5bae46d5f03027c9eeeac44b57deb0291
Tom <thomas.verin@sonarsource.com>
2017-02-07 10:54:28 +0100

    debug maven

--- travis.sh
+++ travis.sh
@@ -17,9 +17,9 @@ mkdir -p ~/maven
 cp -R /usr/local/maven-3.2.5 ~/maven
 export M2_HOME=~/maven/apache-maven-3.2.5
 export PATH=$M2_HOME/bin:$PATH
-echo $M2_HOME
-echo $PATH
+ls $M2_HOME
 which mvn
+mkdir -p $M2_HOME/lib/ext
 ls $M2_HOME/lib/ext
 
 cp tesla-profiler-0.0.3.jar ~/maven/maven-3.2.5/lib/ext
