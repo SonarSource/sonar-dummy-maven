37db8da4b6574f53a73d26ebc92c1f5a6a6cb988
Tom <thomas.verin@sonarsource.com>
2017-02-07 10:39:37 +0100

    copy mvn to install maven profiler

--- travis.sh
+++ travis.sh
@@ -12,7 +12,13 @@ installTravisTools
 . installJDK8
 
 curl -O http://repo.maven.apache.org/maven2/io/tesla/profile/tesla-profiler/0.0.3/tesla-profiler-0.0.3.jar
-cp tesla-profiler-0.0.3.jar /usr/local/maven-3.2.5/lib/ext
+
+mkdir -p ~/maven
+cp -R /usr/local/maven-3.2.5 ~/maven
+export M2_HOME=~/maven/apache-maven-3.2.5
+export PATH=$M2_HOME/bin:$PATH
+
+cp tesla-profiler-0.0.3.jar ~/maven/maven-3.2.5/lib/ext
 
 java -version
 
