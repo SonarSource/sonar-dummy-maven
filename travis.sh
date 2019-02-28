1da8583a94e6f98499f5db4ab1089c865db417a0
David Rautureau <david.rautureau@sonarsource.com>
2019-02-28 11:42:11 +0100

    Move to Artifactory Cloud (#29)

--- travis.sh
+++ travis.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 function installTravisTools {
   mkdir -p ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v53 | tar zx --strip-components 1 -C ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v54 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
 }
 
@@ -12,24 +12,8 @@ installTravisTools
 cancel_branch_build_with_pr || if [[ $? -eq 1 ]]; then exit 0; fi
 . installJDK8
 
-case "$TEST" in
+#deploy pull request artifacts to Artifactory to start QA
+export DEPLOY_PULL_REQUEST=true
 
-ci)
-  #deploy pull request artifacts to repox to start QA
-  export DEPLOY_PULL_REQUEST=true  
-  CURRENT_VERSION=`maven_expression "project.version"`
-
-  . set_maven_build_version $TRAVIS_BUILD_NUMBER
-  
-  export MAVEN_OPTS="-Xmx1536m -Xms128m"
-  regular_mvn_build_deploy_analyze -Pstats
-
-  cat stats.log      
-  ;;
-
-*)
-  echo "Unexpected TEST mode: $TEST"
-  exit 1
-  ;;
-
-esac
+export MAVEN_OPTS="-Xmx1536m -Xms128m"
+regular_mvn_build_deploy_analyze -Pstats
