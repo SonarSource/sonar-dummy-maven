87b33c677921d903e58b32afbf8f82ed729a58b7
Tom <thomas.verin@sonarsource.com>
2019-09-03 10:31:38 +0200

    define status relative to branch

--- promote.py
+++ promote.py
@@ -4,19 +4,26 @@ print("PULL_REQUESTinPR: " + os.environ['PULL_REQUEST'])
 print("GITHUB_BRANCH: " + os.environ['GITHUB_BRANCH'])
 print("GITHUB_REPO: " + os.environ['GITHUB_REPO'])
 print("BUILD_NUMBER: " + os.environ['BUILD_NUMBER'])
-"""
-if os.environ['PULL_REQUEST']:
-  PRIVATE_TARGET_REPO='sonarsource-private-dev'
-  PUBLIC_TARGET_REPO="sonarsource-public-dev"
-  STATUS="it-passed-pr"
-else:
-  if [[ "$GITHUB_BRANCH" == "master" ]] || [[ "$GITHUB_BRANCH" == "branch-"* ]]; then
-    PRIVATE_TARGET_REPO='sonarsource-private-builds'
-    PUBLIC_TARGET_REPO="sonarsource-public-builds"
-    STATUS="it-passed"
-  fi
-fi
 
+privateTargetRepo=None
+publicTargetRepo=None
+status=None
+
+if os.environ['PULL_REQUEST']:
+  print("in PR")
+  privateTargetRepo='sonarsource-private-dev'
+  publicTargetRepo="sonarsource-public-dev"
+  status="it-passed-pr"
+else:
+  if os.environ['GITHUB_BRANCH'] == "master" or os.environ['GITHUB_BRANCH'].startswith("branch-"):
+    print("NOT in PR")
+    privateTargetRepo='sonarsource-private-builds'
+    publicTargetRepo="sonarsource-public-builds"
+    status="it-passed"
+
+print(status)
+
+"""  
 if [[ "${STATUS:-}" ]]; then
   echo "Promoting build $CIRRUS_REPO_NAME#$BUILD_NUMBER"
   json_payload='{
