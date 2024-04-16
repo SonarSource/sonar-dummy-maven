1678dae52ac14ea51c4182bc199bbe054ba427f6
Mate Molnar <matemoln@users.noreply.github.com>
2024-04-16 14:31:24 +0200

    chore(deps): update parent pom to 63.0.0.1294 (#247)
    * update parent pom to 63.0.0.1294
    * update license headers
    * fix SonarCloud issues
    * chore(BUILD-4602): use the master branch for the release action (#242)

--- sonar-dummy-plugin/src/main/java/com/sonarsource/dummy/plugin/DummyPlugin.java
+++ sonar-dummy-plugin/src/main/java/com/sonarsource/dummy/plugin/DummyPlugin.java
@@ -1,27 +1,29 @@
 /*
- * Copyright (C) 2014-2023 SonarSource SA
+ * Copyright (C) 2014-2024 SonarSource SA
  * All rights reserved
  * mailto:info AT sonarsource DOT com
  */
 package com.sonarsource.dummy.plugin;
 
 import java.util.List;
+import java.util.logging.Logger;
 import org.sonar.api.SonarPlugin;
 import com.google.common.collect.Lists;
 
 public final class DummyPlugin extends SonarPlugin {
 
-  @SuppressWarnings({"rawtypes", "unchecked"})
+  private static final Logger LOGGER = Logger.getLogger(DummyPlugin.class.getName());
+
+  @SuppressWarnings({ "rawtypes" })
   @Override
   public List getExtensions() {
     return Lists.newArrayList();
   }
 
-
-  public String sayHello() {
-    System.out.println("hello");
-    System.out.println("world");
-    return "hello world";
+  public static String sayHello() {
+    String message = "hello world";
+    LOGGER.info(message);
+    return message;
   }
 
 }
